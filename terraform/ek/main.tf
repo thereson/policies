1bba2b7a96a2d709ebd38035b3716059e3c0f22a
thereson <gerardokoli09@gmail.com>
2025-04-30 15:37:48 -0400

    created eks cluster

---
+++ terraform/ek/main.tf
@@ -0,0 +1,178 @@
+
+
+provider "aws" {
+  region = "us-west-2" 
+}
+
+terraform {
+  backend "s3"{
+	bucket = "thereson-pdk"
+	key = "eks/terraform.tfstate"
+	region = "eu-north-1"
+}
+}
+
+
+
+# Create VPC for EKS
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+# EKS Cluster IAM Role
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.28" # Specify your desired Kubernetes version
+
+  vpc_config {
+    subnet_ids = concat(
+      module.vpc.public_subnets,
+      module.vpc.private_subnets
+    )
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+# Node Group IAM Role
+resource "aws_iam_role" "eks_node_group" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+  capacity_type  = "ON_DEMAND"
+
+  depends_on = [
+    aws_eks_cluster.cluster,
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_readonly,
+  ]
+}
+
+# Kubernetes provider configuration
+provider "kubernetes" {
+  host                   = aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      aws_eks_cluster.cluster.name
+    ]
+  }
+}
+
+# Outputs
+output "cluster_name" {
+  value = aws_eks_cluster.cluster.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.cluster.endpoint
+}
+
+output "cluster_certificate_authority" {
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.node_group.node_group_name
+}
--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -14,10 +14,31 @@ resource "aws_iam_role" "example_role" {
     ]
   })
 
-#   tags = {
-#     Environment = "Production"
-#   }
 }
+
+resource "aws_iam_role" "Lambda_execution_role" {
+  name  = "lambda_exec_role"
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com" 
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_layer_version" "node_modules" {
+  layer_name = "node_modules"
+  filename = "../lambda/layers/jwt_layer.zip"
+  compatible_runtimes = [ "nodejs14.x" ]
+  
+}
+
 
 # Attach a managed policy (e.g., S3 ReadOnly)
 resource "aws_iam_role_policy_attachment" "s3_readonly" {

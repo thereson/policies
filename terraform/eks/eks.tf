4d9b50ff2c2d737ae1cbd67589a96a244bc411be
thereson <gerardokoli09@gmail.com>
2025-05-01 04:28:05 -0400

    created other files

--- terraform/eks/eks.tf
+++ terraform/eks/eks.tf
@@ -1,15 +1,3 @@
-provider "aws" {
-  region = "eu-north-1"
-}
-
-terraform {
-  backend "s3" {
-    bucket = "thereson-pdk"
-    key    = "eks/terraform.tfstate"
-    region = "eu-north-1"
-  }
-}
-
 # ----------------------------
 # VPC Resources (Manual Setup)
 # ----------------------------

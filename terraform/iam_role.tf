32bf55ee2b2c32930bb936e530f2b3f159ad72f7
thereson <gerardokoli09@gmail.com>
2025-05-22 10:31:43 -0400

    proper file path

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -82,7 +82,7 @@ resource "aws_iam_role_policy_attachment" "s3_readonly" {
 
 data "archive_file" "lambda_layer_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda/layer"
+  source_dir  = "${path.module}/lambda/layers"
   output_path = "${path.module}/lambda/layer.zip"
 }
 

51751476e0e1cc02ec1005105599511c1e17e825
thereson <gerardokoli09@gmail.com>
2025-05-23 12:38:14 -0400

    fixed

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -67,7 +67,7 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "../src"
-  output_path = "../src/custom.zip"
+  output_path = "../src/index.zip"
 }
 
 

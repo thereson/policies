6d097140f626bc9ca7d542c01097ad302a8710d0
thereson <gerardokoli09@gmail.com>
2025-05-25 15:42:02 -0400

    destroy

--- src/index.py
+++ src/index.py
@@ -1,15 +1,6 @@
-# index.py
-from custom import generate_presigned_url
-
 def lambda_handler(event, context):
-    bucket = "my-test-bucket"
-    key = "example-folder/test-file.txt"
-    expiration_seconds = 600
-
-    url = generate_presigned_url(bucket, key, expiration_seconds)
-
-    return {
-        "statusCode": 200,
-        "body": f"Hello from Lambda!",
-        "presigned_url": url
-    }
+    try:
+        from PIL import Image
+        return {"ok": True}
+    except Exception as e:
+        return {"error": str(e)}
